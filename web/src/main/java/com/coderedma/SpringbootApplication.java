d518a6772a4a5a87ad8ae7f4125b50d3cde46c15
coderedma <3114867971@qq.com>
2023-03-09 21:50:42 +0800

    create module -- coderedma commit

--- web/src/main/java/com/coderedma/SpringbootApplication.java
+++ web/src/main/java/com/coderedma/SpringbootApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElectronicAlbumApplication {
-
+public class SpringbootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ElectronicAlbumApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
-
 }
