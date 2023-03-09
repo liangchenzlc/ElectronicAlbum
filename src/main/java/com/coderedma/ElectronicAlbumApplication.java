d5591081fec7bf38c88e55627b368aa8a2f651e6
coderedma <3114867971@qq.com>
2023-03-09 13:15:32 +0800

    init project

---
+++ src/main/java/com/coderedma/ElectronicAlbumApplication.java
@@ -0,0 +1,13 @@
+package com.coderedma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicAlbumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicAlbumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coderedma/ElectronicAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderedma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicAlbumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
